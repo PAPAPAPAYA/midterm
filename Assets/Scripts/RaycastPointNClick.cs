e41a09455c216126e5f2532070f4791511fa9fdb
PAPAPAPAYA <jy2636@nyu.edu>
2019-10-02 18:46:15 -0400

    transition between off-screen and object examine is done

--- Assets/Scripts/RaycastPointNClick.cs
+++ Assets/Scripts/RaycastPointNClick.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class RaycastPointNClick : MonoBehaviour
 {
+    // for transition between screen and off-screen
+    [Header("on/off screen")]
     public bool onScreen = false;
     public GameObject onScreenPos;
     public GameObject defaultPos;   
     public float defaultXAngle = 0f;
     public float screenXAngle = 0f;
     public float smooth = 0f; // variable to smoothen the slerp
+    //---------------------------------------------------------
+    
+    // for transition between off-screen and object examine
+    [Header("on/off object")]
+    public bool onObject = false;
+    public GameObject daObject;
+    public GameObject examinePos;
+    public GameObject objectDefaultPos;
+    public float examineSmooth;
+    //--------------------------------------------------------
+
+
+
     // Update is called once per frame
     void Update()
     {
@@ -18,11 +33,20 @@ public class RaycastPointNClick : MonoBehaviour
             transform.position = Vector3.Lerp(transform.position,onScreenPos.transform.position,smooth);
             Quaternion target = Quaternion.Euler(screenXAngle,-90f,0f); // y is -90 because the initial angle is -90
             transform.rotation = Quaternion.Slerp(transform.rotation,target,smooth);
-        }else{
-            // lerp the camera back to default pos
-                transform.position = Vector3.Lerp(transform.position,defaultPos.transform.position,smooth);
-                Quaternion target = Quaternion.Euler(defaultXAngle,-90f,0f); // y is -90 because the initial angle is -90
-                transform.rotation = Quaternion.Slerp(transform.rotation,target,smooth);
+        }else if(!onScreen) {
+            // lerp the camera back to default pos\
+            transform.position = Vector3.Lerp(transform.position, defaultPos.transform.position,smooth);
+            Quaternion target = Quaternion.Euler(defaultXAngle,-90f,0f); // y is -90 because the initial angle is -90
+            transform.rotation = Quaternion.Slerp(transform.rotation,target,smooth);
+        }
+        if (onObject){
+            daObject.transform.position = Vector3.Lerp(daObject.transform.position, examinePos.transform.position, examineSmooth);
+
+            // if (Input.GetMouseButton(1)){
+            //     onObject = false;
+            // }
+        }else if (!onObject && daObject != null){
+            daObject.transform.position = Vector3.Lerp(daObject.transform.position, objectDefaultPos.transform.position, examineSmooth);
         }
 
 
@@ -39,21 +63,29 @@ public class RaycastPointNClick : MonoBehaviour
         Debug.DrawRay(mouseRay.origin,mouseRay.direction * mouseRayDist, Color.magenta);
 
         // STEP 4: shoot the raycast
+
+        
+        
         if (Physics.Raycast(mouseRay,out rayHit, mouseRayDist)){
-            if (Input.GetMouseButton(0) && rayHit.collider.gameObject.layer == 8){
+            if (Input.GetMouseButton(0) && rayHit.collider.gameObject.layer == 8){// if the player click on the screen, enter the screen focus position
                 onScreen = true;
             }
-        }
-        // if the player clicks outside the screen
-        if (Physics.Raycast(mouseRay,out rayHit, mouseRayDist)){
-            if (Input.GetMouseButton(0) && rayHit.collider.gameObject.layer != 8){
-                print("back");
+            if (Input.GetMouseButton(0) && rayHit.collider.gameObject.layer != 8){// if the player clicks outside the screen, exit the screen focus position
                 onScreen = false;
-                
+            }
+            if (Input.GetMouseButton(0) && rayHit.collider.gameObject.layer == 9){
+                onObject = true;
+                daObject = rayHit.collider.gameObject;
+            }
+            if (Input.GetMouseButton(0) && rayHit.collider.gameObject.layer != 9 && onObject == true){
+                onObject = false;
+                daObject.transform.position = Vector3.Lerp(daObject.transform.position, objectDefaultPos.transform.position, examineSmooth);
             }
         }
     }
 }
 // TODO
 // transit from off-screen to on-screen...done
-// transit from on-screen to off-screen
+// transit from on-screen to off-screen...done
+// transit between off-screen and object examine position...done
+// rotate the object in object examine position
